b39338c54d2588c48aaf7a21599a61da6aee7c6b
lxjn <1056227556@qq.com>
2020-12-25 11:46:28 +0800

    配置拦截器

---
+++ src/main/java/com/lxjn/hgd/config/GlobalInterceptor.java
@@ -0,0 +1,58 @@
+package com.lxjn.hgd.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 通用请求拦截器
+ */
+@Slf4j
+@Component
+public class GlobalInterceptor implements HandlerInterceptor {
+
+    /**
+     * 在目标方法执行前执行
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("请求预处理...");
+        return true;
+    }
+
+    /**
+     * 在目标方法执行后执行，但在请求返回前，我们仍然可以对 ModelAndView进行修改
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("请求后处理...");
+    }
+
+    /**
+     * 在请求已经返回之后执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("请求结果返回后处理...");
+    }
+}
+
--- src/main/java/com/lxjn/hgd/config/WebConfigurer.java
+++ src/main/java/com/lxjn/hgd/config/WebConfigurer.java
@@ -1,11 +1,17 @@
 package com.lxjn.hgd.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * web配置类,可以实现以下功能配置
+ * 继承 WebMvcConfigurationSupport 或实现 WebMvcConfigurer 接口
+ * web拦截器配置类,可以实现以下功能配置
  * addInterceptors：拦截器
  * addViewControllers：页面跳转
  * addResourceHandlers：静态资源
@@ -15,12 +21,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * addCorsMappings：跨域
  * configureMessageConverters：信息转换器
  */
-public class WebConfigurer implements WebMvcConfigurer {
+@Slf4j
+@Configuration
+public class WebConfigurer extends WebMvcConfigurationSupport {
     @Value(value = "${my.uploadpath}")
     private String m_uploadPath; // 文件上传的物理路径
 
     @Value(value = "${my.uploadurl}")
     private String m_uploadUrl; // 文件访问的url路径
+
+    @Autowired
+    GlobalInterceptor globalInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        log.info("正在加载请求拦截器...");
+        registry.addInterceptor(globalInterceptor).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+
+
+
 
     /**
      * 静态资源配置类
@@ -32,14 +53,17 @@ public class WebConfigurer implements WebMvcConfigurer {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/**").addResourceLocations("/static/");
-//
-//        // upload path:
-//        String uploadPath = "file:" + m_uploadPath + "/";
-//        String uploadUrl = m_uploadUrl + "/**";
-//
-//        registry.addResourceHandler(uploadUrl).addResourceLocations(uploadPath);
-//
-//        super.addResourceHandlers(registry);
+        log.info("加载静态资源拦截器...");
+        String os = System.getProperty("os.name");
+        // 判断文件路径写法是否与操作系统相符
+        if ((os.toLowerCase().startsWith("win")&&!m_uploadPath.startsWith("/")) ||
+                (os.toLowerCase().startsWith("linux")&&m_uploadPath.startsWith("/")) ){
+            registry.addResourceHandler(m_uploadUrl).addResourceLocations("file:"+m_uploadPath);
+
+            super.addResourceHandlers(registry);
+        }else {
+            log.warn("操作系统类型与自定义静态资源上传路径格式不符，自定义静态文件映射失效！");
+        }
+
     }
 }
